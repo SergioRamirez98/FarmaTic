c614b92eb3f572c802a313b9dfe3f8f2c98059d9
SergioRamirez98 <ramirez.98.sergio@gmail.com>
2024-12-29 18:50:05 -0300

    se corrige tamaño de formulario de ventas

--- Farmatic/Vista/FormulariosMenu/Ventas/CV_Ventas.Designer.cs
+++ Farmatic/Vista/FormulariosMenu/Ventas/CV_Ventas.Designer.cs
@@ -277,7 +277,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(220)))), ((int)(((byte)(240)))));
-            this.ClientSize = new System.Drawing.Size(1656, 735);
+            this.ClientSize = new System.Drawing.Size(1656, 769);
             this.Controls.Add(this.Btn_Refrescar);
             this.Controls.Add(this.Pnl_Catalogo);
             this.Controls.Add(this.Pnl_Carrito);
